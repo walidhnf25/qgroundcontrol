45f10e744cf8af0a646fe0f78a335d5d658ff69d
Domenico Patella <dome2870@gmail.com>
2021-04-04 09:36:00 +0200

    Custom Support APM Firmware

--- custom/src/AutoPilotPlugin/CustomAutoPilotPlugin.h
+++ custom/src/AutoPilotPlugin/CustomAutoPilotPlugin.h
@@ -11,7 +11,7 @@
  */
 
 #pragma once
-
+/*
 #include "PX4AutoPilotPlugin.h"
 #include "Vehicle.h"
 
@@ -29,4 +29,72 @@ private slots:
 private:
     QVariantList _components;
 
+};*/
+
+
+
+
+#ifndef APMAutoPilotPlugin_H
+#define APMAutoPilotPlugin_H
+
+#include "AutoPilotPlugin.h"
+#include "Vehicle.h"
+
+class APMAirframeComponent;
+class APMFlightModesComponent;
+class APMRadioComponent;
+class APMTuningComponent;
+class APMSafetyComponent;
+class APMSensorsComponent;
+class APMPowerComponent;
+class APMMotorComponent;
+class APMCameraComponent;
+class APMLightsComponent;
+class APMSubFrameComponent;
+class ESP8266Component;
+class APMHeliComponent;
+class APMFollowComponent;
+
+/// This is the APM specific implementation of the AutoPilot class.
+class CustomAutoPilotPlugin : public AutoPilotPlugin
+{
+    Q_OBJECT
+
+public:
+    CustomAutoPilotPlugin(Vehicle* vehicle, QObject* parent);
+    ~CustomAutoPilotPlugin();
+
+    // Overrides from AutoPilotPlugin
+    const QVariantList& vehicleComponents(void) override;
+    QString prerequisiteSetup(VehicleComponent* component) const override;
+
+protected:
+    bool                        _incorrectParameterVersion; ///< true: parameter version incorrect, setup not allowed
+    APMAirframeComponent*       _airframeComponent;
+    APMCameraComponent*         _cameraComponent;
+    APMLightsComponent*         _lightsComponent;
+    APMSubFrameComponent*       _subFrameComponent;
+    APMFlightModesComponent*    _flightModesComponent;
+    APMPowerComponent*          _powerComponent;
+    APMMotorComponent*          _motorComponent;
+    APMRadioComponent*          _radioComponent;
+    APMSafetyComponent*         _safetyComponent;
+    APMSensorsComponent*        _sensorsComponent;
+    APMTuningComponent*         _tuningComponent;
+    ESP8266Component*           _esp8266Component;
+    APMHeliComponent*           _heliComponent;
+#if 0
+    // Follow me not ready for Stable
+    APMFollowComponent*         _followComponent;
+#endif
+
+#if !defined(NO_SERIAL_LINK) && !defined(__android__)
+private slots:
+    void _checkForBadCubeBlack(void);
+#endif
+
+private:
+    QVariantList                _components;
 };
+
+#endif
